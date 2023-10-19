d438ffaae2454f279aca9b51d6b80183b6e23133
Bethwel Njenga <bethwelnjenga237@gmail.com>
2023-10-19 23:33:56 +0300

    Update singlepost.jsx

--- client/src/singlepost/singlepost.jsx
+++ client/src/singlepost/singlepost.jsx
@@ -4,6 +4,7 @@ function Singlepost() {
   return (
     <div className='singlePost'>
       <div className="singlepostWrapper">
+
         
         <img src='../imagenew5.jpg' alt="" className='singlepostImage'/>
         <h1 className="singlepostTitle">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -13,6 +14,7 @@ function Singlepost() {
         </div> 
         </h1>
         <span className="singlepostAuthor">Author:<b>Florence</b></span>
+        
         <span className="singlepostDate">1 hour</span>
       </div>
       <p className='singlepostInfo'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, similique iure accusamus ex cumque adipisci dolores possimus perspiciatis quo dolorum alias id, reiciendis nobis quos iusto totam doloribus magni ad. Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quasi esse voluptatem aut? Ducimus architecto, asperiores suscipit distinctio a officiis nobis fugit commodi, obcaecati numquam vel non alias repellendus maiores? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, mollitia debitis impedit amet voluptatibus, dolor magni ratione labore corporis quidem accusantium dolores aliquid dicta voluptas maiores unde vero. Consequatur, nobis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio quidem ullam consequuntur dolor itaque modi maiores sequi aut quibusdam reiciendis aliquid magnam labore repellat ratione nostrum expedita possimus, quam molestiae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quasi obcaecati totam perferendis quas voluptas itaque, aliquid dignissimos aut dolor officia rerum libero ea earum repellat? Eligendi eos ullam culpa? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse quos numquam hic aliquid aspernatur laborum natus cumque, nisi ea odit harum praesentium impedit aut, reprehenderit fugiat id sit possimus? Vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione dicta atque nesciunt aperiam laudantium quod ab perferendis pariatur soluta consequatur quis temporibus id sint reiciendis, ea, facilis dolore fuga obcaecati? </p>
